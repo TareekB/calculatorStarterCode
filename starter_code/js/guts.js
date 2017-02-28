b60c078aada7fe35c0db52c4957f00006dc23041
TareekB <tareek.blanchard@envirostudies.org>
2017-02-28 22:23:05 +0000

    calc functions and started cbroot

--- starter_code/js/guts.js
+++ starter_code/js/guts.js
@@ -5,41 +5,48 @@
 
 function divide(x,y)
 {
+	return x/y;
 	//Write your code below this line
 	
 }
 
 function multiply(x,y)
-{
+{ return x*y;
 	
 }
 
 function subtract(x,y)
-{
+{return x-y;
 	
 }
 
 function add(x,y)
-{
+{return x+y;
 	
 }
 
 function power(x,y)
-{
+{     return Math.pow(x,y);
 	
 }
 
 function square_root(x)
-{
+{ return Math.sqrt(x);
 	
 }
 
 function logBase10(x)
-{
+{  return Math.log(x);
+
 	
 }
 
 function generateRandomNumber()
-{
+{ return Math.random();
 	
+}
+
+function cubeRoot(x)
+{
+    return Math.cbrt(x);
 }
